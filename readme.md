9fae03d803a530db2fc1d78d212684d832e22f15
Vivek Dubey <dubey.vivek.2805@gmail.com>
2020-08-02 00:46:43 +0530

    Added various features as mentioned in the readme file

--- readme.md
+++ readme.md
@@ -3,15 +3,22 @@ This is the famous snake game, implemented in JavaScript. The game rules are sim
 ```javascript
         //check collision with self
         if(this.position[0].x == this.position[i].x && this.position[0].y == this.position[i].y) {
-            // add rules here as the snake has bite itself
-            if(this.position.length > 2) {
-                // splice modifies the array
-                this.position.splice(1);
+                // add rules here as the snake has bite itself
+                if(this.position.length > 2) {
+                    // splice modifies the array
+                    // this.position.splice(1);
+                    window.alert("Game Over! Your score is " + String(score));
+                    this.hasSnakeBit = true;
+                }
+                break;
             }
-            break;
-        }
 ```
 ## Work under progress :
-1. Enabling touch features and making it mobile-friendly.
-2. A scoring system.
-3. Adding message features. 
+1. ~A scoring system~
+2. ~Adding alert features~
+3. ~Adding some style~
+4. ~Speed increase after a certain interval~
+5. ~Added navigation pane~
+6. Multiple food available
+7. User name input
+8. Enabling touch features and making it mobile-friendly.
--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const cvs = document.getElementById("myCanvas");
 const ctx = cvs.getContext("2d");
 
-// height of the canvas
-cvs.width = 400;
+// // height of the canvas
+cvs.width = 600;
 cvs.height = 400;
 
 //counting each frames 
 // this will help control the speed
 let frames = 0;
 let score = 0;
+let distance = 0;
+let speed = 12;
 
 const direction = {
     currrent: 0,
@@ -56,7 +58,7 @@ const food = {
 
     draw : function() {
         ctx.beginPath();
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "green";
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         ctx.fill();
         ctx.closePath();
@@ -64,6 +66,7 @@ const food = {
 }
 
 const snake = {
+    hasSnakeBit : false,
     radius : 10,
     position : [{x : cvs.width / 2, y : cvs.height / 2}],
     velocity: 20,
@@ -77,10 +80,12 @@ const snake = {
             ctx.closePath();
         }
     },
+
     checkCollision: function(snakeX, snakeY) {
         // checking if snake's body touches the food
         return (getDistance(snakeX, snakeY, food.x, food.y) < 2 * this.radius);
     },
+
     allocateNewFood: function(snakeX, snakeY) {
         food.x = Math.random() * cvs.width;
         food.y = Math.random() * cvs.height;
@@ -89,65 +94,93 @@ const snake = {
         if(this.position.some(item => this.checkCollision(item.x, item.y)))
             this.allocateNewFood(snakeX, snakeY);
     },
+
+    updateDistance: function() {
+        if(direction.current !== undefined) {
+            distance ++;
+            document.getElementById('distance').innerHTML = "Distance : " + String(distance);    
+        }
+    },
+
+    movePiecesTogether: function() {
+        for(let i = this.position.length - 1; i > 0; i--) {
+            //check collision with self
+            if(this.position[0].x == this.position[i].x && this.position[0].y == this.position[i].y) {
+                // add rules here as the snake has bite itself
+                if(this.position.length > 2) {
+                    // splice modifies the array
+                    // this.position.splice(1);
+                    window.alert("Game Over! Your score is " + String(score));
+                    this.hasSnakeBit = true;
+                }
+                break;
+            }
+
+            // moving the body together
+            this.position[i].x = this.position[i - 1].x;
+            this.position[i].y = this.position[i - 1].y; 
+        }
+    },
+
+    updateDirection: function() {
+        //updating as per the current direction of the snake
+        if(direction.current == direction.right) {
+            this.position[0].x += this.velocity;
+        }
+        if(direction.current == direction.left) {
+            this.position[0].x -= this.velocity;
+        } 
+        if(direction.current == direction.up) {
+            this.position[0].y += this.velocity;
+        }
+        if(direction.current == direction.down) {
+            this.position[0].y -= this.velocity;
+        }
+    },
+
+    didAteFood: function() {
+        //collision with food
+        if(this.checkCollision(this.position[0].x, this.position[0].y)) {
+            // console.log("collision");
+            // new food
+
+            //update and reflect score
+            score += 10 + Math.ceil(Math.sqrt(distance));
+            document.getElementById('score').innerHTML = "Score : " + String(score);
+
+            //allocating new food
+            this.allocateNewFood(this.position[0].x, this.position[0].y);
+            
+
+            // increase snake length from the last
+            this.position.push({x : this.position[this.position.length - 1].x, y : this.position[this.position.length - 1].y })
+        }
+    },
+
+    wrapUp: function() {
+        // giving a wrap up property
+        if(this.position[0].x < 0) this.position[0].x = cvs.width - this.radius;
+        if(this.position[0].x > cvs.width) this.position[0].x = this.radius;
+        if(this.position[0].y < 0) this.position[0].y = cvs.height - this.radius;
+        if(this.position[0].y > cvs.height) this.position[0].y = this.radius;
+    },
+
+    speedUp: function() {
+        if(speed > 1 && distance % 150 == 0)
+            speed--;
+    },
+
     update : function() {
         
         // move the snake
         // updating at multiples of 6
-        if(frames % 6 == 0) {
-
-            for(let i = this.position.length - 1; i > 0; i--) {
-
-                //check collision with self
-                if(this.position[0].x == this.position[i].x && this.position[0].y == this.position[i].y) {
-                    // add rules here as the snake has bite itself
-                    if(this.position.length > 2) {
-                        // splice modifies the array
-                        this.position.splice(1);
-                    }
-                    break;
-                }
-
-                // moving the body together
-                this.position[i].x = this.position[i - 1].x;
-                this.position[i].y = this.position[i - 1].y; 
-            }
-
-            //updating as per the current direction of the snake
-            if(direction.current == direction.right) {
-                this.position[0].x += this.velocity;
-            }
-            if(direction.current == direction.left) {
-                this.position[0].x -= this.velocity;
-            } 
-            if(direction.current == direction.up) {
-                this.position[0].y += this.velocity;
-            }
-            if(direction.current == direction.down) {
-                this.position[0].y -= this.velocity;
-            }
-
-            //collision with food
-            if(this.checkCollision(this.position[0].x, this.position[0].y)) {
-                // console.log("collision");
-                // new food
-
-                //update and reflect score
-                score += 10;
-                document.getElementById('score').innerHTML = score;
-
-                //allocating new food
-                this.allocateNewFood(this.position[0].x, this.position[0].y);
-
-                // increase snake length from the last
-                this.position.push({x : this.position[this.position.length - 1].x, y : this.position[this.position.length - 1].y })
-            }
-
-            // giving a wrap up property
-            if(this.position[0].x < 0) this.position[0].x = cvs.width - this.radius;
-            if(this.position[0].x > cvs.width) this.position[0].x = this.radius;
-            if(this.position[0].y < 0) this.position[0].y = cvs.height - this.radius;
-            if(this.position[0].y > cvs.height) this.position[0].y = this.radius;
-
+        if(frames % speed == 0) {
+            this.speedUp();
+            this.updateDistance();
+            this.movePiecesTogether();
+            this.updateDirection();
+            this.didAteFood();
+            this.wrapUp();
         }        
     }
 }
@@ -155,16 +188,21 @@ const snake = {
 function getDistance(snakeX, snakeY, foodX, foodY) {
     let distanceX = foodX - snakeX;
     let distanceY = foodY - snakeY;
-    // Pythagoras theorem
+    // Pythagoras theorem : distance = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
     return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
 }
 
 function main() {
-    ctx.clearRect(0, 0, cvs.clientWidth, cvs.height);
-    snake.update();
-    snake.draw();
-    food.draw();
-    frames ++;
-    requestAnimationFrame(main);    
+    if(snake.hasSnakeBit == false) {
+        ctx.clearRect(0, 0, cvs.clientWidth, cvs.height);
+        snake.update();
+        snake.draw();
+        food.draw();
+        frames ++;
+        requestAnimationFrame(main);        
+    }
+    else {
+        document.getElementById('gameover').innerHTML = "Please refresh page to restart!";
+    }
 }
 requestAnimationFrame(main);
